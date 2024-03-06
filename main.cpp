5201453e643e9f3a48b1885b9bf2d2f324df76e3
ShivgoudaPatil <135003713+ShivgoudaPatil@users.noreply.github.com>
2024-03-06 12:13:41 +0530

    main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG21CS562\n";
+  return 0;
+}
