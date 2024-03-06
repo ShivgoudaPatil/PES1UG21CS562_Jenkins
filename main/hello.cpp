81756b45e9ffec6cd0e4068a35aac6bed8e6ba92
H K Patil <shivgouda.h.patil@gmail.com>
2024-03-06 11:05:48 +0530

    thirdcommit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(void) {
-  //change in main
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Update\n";
   return 0;
 }
